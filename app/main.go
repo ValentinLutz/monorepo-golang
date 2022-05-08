b2fe23ec6b9f73122dca585f76c09f5d015c169c
ValentinLutz <allround.email@gmail.com>
2022-05-08 18:59:11 +0200

    Add graceful shutdown of server

--- app/main.go
+++ app/main.go
@@ -5,12 +5,16 @@ import (
 	"app/external/database"
 	"app/internal"
 	"app/serve"
+	"context"
 	"flag"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/julienschmidt/httprouter"
 	"github.com/rs/zerolog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -21,6 +25,7 @@ var (
 )
 
 func main() {
+	flag.Parse()
 	logger := internal.NewLogger()
 
 	newConfig, err := internal.NewConfig(configFile)
@@ -38,14 +43,40 @@ func main() {
 
 	server := NewServer(&logger, &newConfig, db)
 
+	go StartServer(server, &logger)
+	GracefulServerShutdown(server, &logger)
+}
+
+func StartServer(server *http.Server, logger *zerolog.Logger) {
 	logger.Info().
 		Str("address", server.Addr).
 		Msg("Starting server")
-	err = server.ListenAndServeTLS(certFile, keyFile)
-	if err != nil {
+	err := server.ListenAndServeTLS(certFile, keyFile)
+	if err != http.ErrServerClosed {
 		logger.Fatal().
 			Err(err).
 			Msg("Failed to start server")
+	}
+
+}
+
+func GracefulServerShutdown(server *http.Server, logger *zerolog.Logger) {
+	osSignal := make(chan os.Signal, 1)
+	signal.Notify(osSignal, os.Interrupt, syscall.SIGTERM)
+	<-osSignal
+
+	timeout := time.Second * 20
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	logger.Info().Float64("timeout", timeout.Seconds()).Msg("Stopping server")
+	err := server.Shutdown(ctx)
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Msg("Failed to shutdown server")
+	} else {
+		logger.Info().Msg("Server stopped")
 	}
 }
 
