6d058ca8922315ab3034b0c054eb188d3fbfc6ef
ValentinLutz <allround.email@gmail.com>
2022-12-04 22:35:58 +0100

    Add index for order id in order item table

--- src/go/services/order/migration-database/migration/V2.1__create_order_item_table.sql
+++ src/go/services/order/migration-database/migration/V2.1__create_order_item_table.sql
@@ -6,11 +6,14 @@ CREATE TABLE order_service.order_item
     order_id      VARCHAR     NOT NULL,
     item_name     VARCHAR     NOT NULL,
     PRIMARY KEY (order_item_id),
-    CONSTRAINT order_id FOREIGN KEY (order_id) REFERENCES order_service.order (order_id)
+    CONSTRAINT order_id_fkey FOREIGN KEY (order_id) REFERENCES order_service.order (order_id)
 );
+
+CREATE INDEX order_item_order_id_idx
+    ON order_service.order_item (order_id);
 
 CREATE TRIGGER update_order_item_modified_date
     BEFORE UPDATE
-    ON order_service.order
+    ON order_service.order_item
     FOR EACH ROW
 EXECUTE PROCEDURE order_service.update_modified_date();
