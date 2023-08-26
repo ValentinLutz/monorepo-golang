91371198c4e64306616a97764c72362292dcbef5
ValentinLutz <allround.email@gmail.com>
2023-08-26 12:36:15 +0200

    Update flyway database migration magefile

--- services/order/magefiles/db.go
+++ services/order/magefiles/db.go
@@ -9,10 +9,12 @@ import (
 	"os"
 )
 
+const flywayDockerImage = "flyway/flyway:9.21.2-alpine"
+
 type Db mg.Namespace
 
-// Migrate flyway schema to the database | PROFILE, FLYWAY_USER, FLYWAY_PASSWORD
-func (Db) Migrate() {
+// Cleans flyway schema from the database | PROFILE, FLYWAY_USER, FLYWAY_PASSWORD
+func (Db) Clean() {
 	flywayUser := getFlywayUserOrDefault()
 	flywayPassword := getFlywayPasswordOrDefault()
 	profile := getProfileOrDefault()
@@ -28,8 +30,30 @@ func (Db) Migrate() {
 		"--network", "host",
 		"--volume", "./migration:/flyway/sql/migration",
 		"--volume", "./"+profile+".conf:/flyway/conf/flyway.conf",
-		"flyway/flyway:9.17.0-alpine",
+		flywayDockerImage,
 		"clean",
+		"-user="+flywayUser,
+		"-password="+flywayPassword,
+	)
+}
+
+// Migrates flyway schema to the database | PROFILE, FLYWAY_USER, FLYWAY_PASSWORD
+func (Db) Migrate() {
+	flywayUser := getFlywayUserOrDefault()
+	flywayPassword := getFlywayPasswordOrDefault()
+	profile := getProfileOrDefault()
+
+	os.Chdir("./migration-database")
+	defer os.Chdir("..")
+	sh.RunV(
+		"docker",
+		"run",
+		"-it",
+		"--rm",
+		"--network", "host",
+		"--volume", "./migration:/flyway/sql/migration",
+		"--volume", "./"+profile+".conf:/flyway/conf/flyway.conf",
+		flywayDockerImage,
 		"migrate",
 		"-user="+flywayUser,
 		"-password="+flywayPassword,
